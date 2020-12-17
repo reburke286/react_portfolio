a509adc2e0a8463a87607d7d192457d761a357c1
reburke286 <53805241+reburke286@users.noreply.github.com>
2020-12-17 14:27:56 -0800

    Updates

---
+++ precache-manifest.f084b6407011888a77b18e8a63a023e8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a26a358988c9cd9d9da37c95fcc866e",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "a8b242c8984a6f86a212",
+    "url": "/react_portfolio/static/css/main.49964776.chunk.css"
+  },
+  {
+    "revision": "c0be5765f9bd1ef55bae",
+    "url": "/react_portfolio/static/js/2.ce7a8655.chunk.js"
+  },
+  {
+    "revision": "ea4b5ae141dc8c71e175204f6e8e684d",
+    "url": "/react_portfolio/static/js/2.ce7a8655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b242c8984a6f86a212",
+    "url": "/react_portfolio/static/js/main.061a7b88.chunk.js"
+  },
+  {
+    "revision": "b30a7b2498e59c802f08",
+    "url": "/react_portfolio/static/js/runtime-main.04f95be2.js"
+  },
+  {
+    "revision": "9e4bfac78c4de3d38b47e4fcb823fa41",
+    "url": "/react_portfolio/static/media/10k.9e4bfac7.jpeg"
+  },
+  {
+    "revision": "e7472750aa600d678f3c75829ac53d7f",
+    "url": "/react_portfolio/static/media/BECCA BURKE (1).e7472750.pdf"
+  },
+  {
+    "revision": "ad2ea19f8c723efd263081448f3b33be",
+    "url": "/react_portfolio/static/media/BecandKate.ad2ea19f.jpeg"
+  },
+  {
+    "revision": "90702ce5cd700b72b7aa268306d19d34",
+    "url": "/react_portfolio/static/media/BurgerThumbnail.90702ce5.JPG"
+  },
+  {
+    "revision": "43a1f3dd16562dfe10bdb1509da1992f",
+    "url": "/react_portfolio/static/media/CoffeeSelfie.43a1f3dd.jpeg"
+  },
+  {
+    "revision": "6d320756b094f2f655bbd3b39d8a84a9",
+    "url": "/react_portfolio/static/media/DayPlannerThumbnail.6d320756.JPG"
+  },
+  {
+    "revision": "936f77623824d896835c322e77618fa7",
+    "url": "/react_portfolio/static/media/DomainNames.936f7762.png"
+  },
+  {
+    "revision": "8ab2324f9a117ce0fe66613ce16b649e",
+    "url": "/react_portfolio/static/media/EatDaBurger.8ab2324f.png"
+  },
+  {
+    "revision": "c61c3cc1d42f0c16a6cc5986716ffc79",
+    "url": "/react_portfolio/static/media/Neil.c61c3cc1.jpeg"
+  },
+  {
+    "revision": "88db5e94c0cfb32fb2911626f871cec0",
+    "url": "/react_portfolio/static/media/SpokeoThumbnail.88db5e94.jpg"
+  },
+  {
+    "revision": "915de071b858278ef48002d5d12089c8",
+    "url": "/react_portfolio/static/media/WeatherApp.915de071.png"
+  },
+  {
+    "revision": "158054d06bf5cf3f8d27f6ad4f9717fa",
+    "url": "/react_portfolio/static/media/WeatherThumbnail.158054d0.JPG"
+  },
+  {
+    "revision": "d2f38794f6a46cd8bf281c37a0e63774",
+    "url": "/react_portfolio/static/media/WorkoutThumbnail.d2f38794.JPG"
+  },
+  {
+    "revision": "abeed6e2de9e675df8f8c4abad243fda",
+    "url": "/react_portfolio/static/media/WorkoutTracker.abeed6e2.png"
+  }
+]);
